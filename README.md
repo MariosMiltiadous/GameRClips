31c99e82704e7d679bfef526da58ed7fd0e3f7cf
mm185821 <mm185821@ncr.com>
2023-06-02 10:25:35 +0300

    Creation of components nav and modal

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Basics
+# Clips
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.2.4.
 
---
+++ src/app/user/auth-modal/auth-modal.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ModalService } from 'src/app/services/modal.service';
+
+@Component({
+  selector: 'app-auth-modal',
+  templateUrl: './auth-modal.component.html',
+  styleUrls: ['./auth-modal.component.css']
+})
+export class AuthModalComponent implements OnInit,OnDestroy {
+
+  constructor(public modal: ModalService){
+  }
+
+  ngOnInit() {
+    this.modal.register('auth'); 
+  }
+
+  ngOnDestroy() {
+   this.modal.unRegister('auth'); 
+  }
+
+}
