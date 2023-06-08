6cf16136ee5ffd789eb70350e9ce61c54f50a84b
mm185821 <mm185821@ncr.com>
2023-06-08 14:18:40 +0300

    Create Register Reactive-form

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,21 +1,37 @@
-import { NgModule } from '@angular/core';
+import { NgModule,ChangeDetectionStrategy  } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal/modal.component';
 import { ModalService } from '../services/modal.service';
-
+import { TabsContainerComponent } from './tabs-container/tabs-container.component';
+import { TabComponent } from './tab/tab.component';
+import { InputComponent } from './input/input.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { provideEnvironmentNgxMask,NgxMaskDirective } from 'ngx-mask';
+import { AlertComponent } from './alert/alert.component';
 
 @NgModule({
   declarations: [
-    ModalComponent
+    ModalComponent,
+    TabsContainerComponent,
+    TabComponent,
+    InputComponent,
+    AlertComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    NgxMaskDirective
   ],
   exports:[
-    ModalComponent
+    ModalComponent,
+    TabsContainerComponent,
+    TabComponent,
+    InputComponent,
+    AlertComponent
   ],
   providers:[
-    ModalService
+    //ModalService
+    provideEnvironmentNgxMask(),
   ]
 
 })
